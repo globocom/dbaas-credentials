08eaefe32e0ec70ccf410aba30c3db2cc9210b39
Ricardo Dias <ricardo.gestec@corp.globo.com>
2014-04-16 11:36:31 -0300

    Fix version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='dbaas_credentials',
-    version='0.5.0',
+    version='0.6.0',
     description='A credential manager for DBaaS integrations',
     long_description=readme + '\n\n' + history,
     author='Felippe da Motta Raposo',
