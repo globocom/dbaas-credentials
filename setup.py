c3fd2218fe5a4dc779cafa9895f4821a2612416c
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-05-16 11:39:25 -0300

    Fix version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='dbaas_credentials',
-    version='0.9.2',
+    version='0.9.3',
     description='A credential manager for DBaaS integrations',
     long_description=readme + '\n\n' + history,
     author='Felippe da Motta Raposo',
