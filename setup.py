4d1470f16a89e13aa39ab6bcd4511f8807c67d76
Ricardo Dias <ricardo@ricardodias.org>
2016-07-06 19:40:15 -0300

    change version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='dbaas_credentials',
-    version='1.1.5',
+    version='1.1.6',
     description='A credential manager for DBaaS integrations',
     long_description=readme + '\n\n' + history,
     author='Felippe da Motta Raposo',
