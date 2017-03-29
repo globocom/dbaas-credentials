73022737455f39d6a65c8ff0dc0c3333ae5f5926
otherpirate <mauro_murari@hotmail.com>
2017-03-29 14:33:52 -0300

    New release: 1.1.13

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='dbaas_credentials',
-    version='1.1.12',
+    version='1.1.13',
     description='A credential manager for DBaaS integrations',
     long_description=readme + '\n\n' + history,
     author='Felippe da Motta Raposo',
