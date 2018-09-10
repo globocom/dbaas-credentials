8a00f0c31f0424bb5172a1abdbb14fb8839b6dd8
elesbom <rafael.goncalves@corp.globo.com>
2018-09-10 11:55:25 -0300

    New version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='dbaas_credentials',
-    version='1.1.17',
+    version='1.1.18',
     description='A credential manager for DBaaS integrations',
     long_description=readme + '\n\n' + history,
     author='Felippe da Motta Raposo',
