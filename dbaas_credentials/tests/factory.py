1c422082f315c148ad61aef9147ebb32ee1855b4
elesbom <rafael.goncalves@corp.globo.com>
2018-11-27 14:56:36 -0200

    Move tests to make then importable from anther apps

--- dbaas_credentials/tests/factory.py
+++ dbaas_credentials/tests/factory.py
@@ -5,24 +5,24 @@ from dbaas_credentials import models
 from physical.tests.factory import EnvironmentFactory
 
 
-class IntegrationTypeCloudStackFactory(factory.DjangoModelFactory):
+class CredentialTypeFactory(factory.DjangoModelFactory):
     FACTORY_FOR = models.CredentialType
 
     name = factory.Sequence(lambda n: 'name_{0}'.format(n))
     type = 1
 
-class IntegrationTypeCloudStackFactory(factory.DjangoModelFactory):
+class CredentialFactory(factory.DjangoModelFactory):
     FACTORY_FOR = models.Credential
 
     user = factory.Sequence(lambda n: 'name_{0}'.format(n))
     password = factory.Sequence(lambda n: 'pass_{0}'.format(n))
-    integration_type = factory.SubFactory(IntegrationTypeCloudStackFactory)
+    integration_type = factory.SubFactory(CredentialTypeFactory)
     token = factory.Sequence(lambda n: 'token_{0}'.format(n))
     secret = factory.Sequence(lambda n: 'secret_{0}'.format(n))
     endpoint =factory.Sequence(lambda n: 'endpoint_{0}'.format(n))
     project = factory.Sequence(lambda n: 'project.glb_{0}'.format(n))
     team = factory.Sequence(lambda n: 'team.glb_{0}'.format(n))
-    
+
     @factory.post_generation
     def environments(self, create, extracted, **kwargs):
         if not create:
