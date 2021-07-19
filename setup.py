dd56de58ea8d759a86bc4f9d27f414de2c71ce49
William Marquardt <william.marquardt@g.globo>
2021-07-19 16:38:11 -0300

    update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='dbaas_credentials',
-    version='1.3.1',
+    version='1.3.2',
     description='A credential manager for DBaaS integrations',
     long_description=readme + '\n\n' + history,
     author='DBaaS',
